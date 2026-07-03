6258722ccfa2a2345d72c5fea35d085434439485
Achira-Boonrath <achirabo@buffalo.edu>
2026-07-03 14:28:04 -0400

    Update costate_Dyn.m

--- codeForMRAC_STParamStudy/costate_Dyn.m
+++ codeForMRAC_STParamStudy/costate_Dyn.m
@@ -4,7 +4,7 @@ a = argsCons.a;
 b = argsCons.b;
 p_min = argsCons.p_min;
 L0 = argsCons.L0;
-rho_s = L0 * argsCons.rho_s;
+rho_s_ = L0 * argsCons.rho_s;
 muEarth = argsCons.muEarth;
 % nPos =argsCons.nPos;
 % nLv = argsCons.nLv;
@@ -14,7 +14,7 @@ Lrx =L(1);Lry =L(2);Lrz =L(3);Lvx =L(4);Lvy =L(5);Lvz =L(6);
 
 p = ( (rx^(2))/(a^(2))+(ry^(2))/(b^(2)) );
 
-f_ = (rx^2/a^2 - p_min + ry^2/b^2);
+f_ = f_;
 if f_ > 0.5
     f_ = 0.5;
 end
@@ -33,19 +33,21 @@ end
 %%
 ds= zeros(1,6);
 %% heaviside quad max
-% consX =Lvx*(muEarth/((rx)^2 + (ry)^2 + (rz)^2)^(3/2) - (3*muEarth*rx*abs(rx)*sign(rx))/((rx)^2 + (ry)^2 + (rz)^2)^(5/2)) + (2*rho_s*rx*dirac((- p_min*a^2*b^2 + a^2*ry^2 + b^2*rx^2)/(a^2*b^2))*f_^2)/a^2 - (4*rho_s*rx*heaviside(-(- p_min*a^2*b^2 + a^2*ry^2 + b^2*rx^2)/(a^2*b^2))*f_)/a^2 - (3*Lvy*muEarth*ry*abs(rx)*sign(rx))/((rx)^2 + (ry)^2 + (rz)^2)^(5/2) - (3*Lvz*muEarth*rz*abs(rx)*sign(rx))/((rx)^2 + (ry)^2 + (rz)^2)^(5/2);
-% consY =Lvy*(muEarth/((rx)^2 + (ry)^2 + (rz)^2)^(3/2) - (3*muEarth*ry*abs(ry)*sign(ry))/((rx)^2 + (ry)^2 + (rz)^2)^(5/2)) + (2*rho_s*ry*dirac((- p_min*a^2*b^2 + a^2*ry^2 + b^2*rx^2)/(a^2*b^2))*f_^2)/b^2 - (4*rho_s*ry*heaviside(-(- p_min*a^2*b^2 + a^2*ry^2 + b^2*rx^2)/(a^2*b^2))*f_)/b^2 - (3*Lvx*muEarth*rx*abs(ry)*sign(ry))/((rx)^2 + (ry)^2 + (rz)^2)^(5/2) - (3*Lvz*muEarth*rz*abs(ry)*sign(ry))/((rx)^2 + (ry)^2 + (rz)^2)^(5/2);
-% consZ =                                                                                                                                                                                                                           Lvz*(muEarth/((rx)^2 + (ry)^2 + (rz)^2)^(3/2) - (3*muEarth*rz*abs(rz)*sign(rz))/((rx)^2 + (ry)^2 + (rz)^2)^(5/2)) - (3*Lvx*muEarth*rx*abs(rz)*sign(rz))/((rx)^2 + (ry)^2 + (rz)^2)^(5/2) - (3*Lvy*muEarth*ry*abs(rz)*sign(rz))/((rx)^2 + (ry)^2 + (rz)^2)^(5/2);
+% consX =Lvx*(muEarth/nPos^(3/2) - (3*muEarth*rx*abs(rx)*sign(rx))/nPos^(5/2)) + (2*rho_s*rx*dirac((- p_min*a^2*b^2 + a^2*ry^2 + b^2*rx^2)/(a^2*b^2))*f_^2)/a^2 - (4*rho_s*rx*heaviside(-(- p_min*a^2*b^2 + a^2*ry^2 + b^2*rx^2)/(a^2*b^2))*f_)/a^2 - (3*Lvy*muEarth*ry*abs(rx)*sign(rx))/nPos^(5/2) - (3*Lvz*muEarth*rz*abs(rx)*sign(rx))/nPos^(5/2);
+% consY =Lvy*(muEarth/nPos^(3/2) - (3*muEarth*ry*abs(ry)*sign(ry))/nPos^(5/2)) + (2*rho_s*ry*dirac((- p_min*a^2*b^2 + a^2*ry^2 + b^2*rx^2)/(a^2*b^2))*f_^2)/b^2 - (4*rho_s*ry*heaviside(-(- p_min*a^2*b^2 + a^2*ry^2 + b^2*rx^2)/(a^2*b^2))*f_)/b^2 - (3*Lvx*muEarth*rx*abs(ry)*sign(ry))/nPos^(5/2) - (3*Lvz*muEarth*rz*abs(ry)*sign(ry))/nPos^(5/2);
+% consZ =                                                                                                                                                                                                                           Lvz*(muEarth/nPos^(3/2) - (3*muEarth*rz*abs(rz)*sign(rz))/nPos^(5/2)) - (3*Lvx*muEarth*rx*abs(rz)*sign(rz))/nPos^(5/2) - (3*Lvy*muEarth*ry*abs(rz)*sign(rz))/nPos^(5/2);
 
 
 %% sigmoid quad max
 % disp(consX - ds(1))
 % disp(consX )
 % disp(ds(1))
-kSig = 100;
-consX =Lvx*(muEarth/((rx)^2 + (ry)^2 + (rz)^2)^(3/2) - (3*muEarth*rx*abs(rx)*sign(rx))/((rx)^2 + (ry)^2 + (rz)^2)^(5/2)) - (4*rho_s*rx*f_)/(a^2*(exp(kSig*f_) + 1)) - (3*Lvy*muEarth*ry*abs(rx)*sign(rx))/((rx)^2 + (ry)^2 + (rz)^2)^(5/2) - (3*Lvz*muEarth*rz*abs(rx)*sign(rx))/((rx)^2 + (ry)^2 + (rz)^2)^(5/2) + (2*kSig*rho_s*rx*exp(kSig*f_)*f_^2)/(a^2*(exp(kSig*f_) + 1)^2);
-consY =Lvy*(muEarth/((rx)^2 + (ry)^2 + (rz)^2)^(3/2) - (3*muEarth*ry*abs(ry)*sign(ry))/((rx)^2 + (ry)^2 + (rz)^2)^(5/2)) - (4*rho_s*ry*f_)/(b^2*(exp(kSig*f_) + 1)) - (3*Lvx*muEarth*rx*abs(ry)*sign(ry))/((rx)^2 + (ry)^2 + (rz)^2)^(5/2) - (3*Lvz*muEarth*rz*abs(ry)*sign(ry))/((rx)^2 + (ry)^2 + (rz)^2)^(5/2) + (2*kSig*rho_s*ry*exp(kSig*f_)*f_^2)/(b^2*(exp(kSig*f_) + 1)^2);
-consZ =Lvz*(muEarth/((rx)^2 + (ry)^2 + (rz)^2)^(3/2) - (3*muEarth*rz*abs(rz)*sign(rz))/((rx)^2 + (ry)^2 + (rz)^2)^(5/2)) - (3*Lvx*muEarth*rx*abs(rz)*sign(rz))/((rx)^2 + (ry)^2 + (rz)^2)^(5/2) - (3*Lvy*muEarth*ry*abs(rz)*sign(rz))/((rx)^2 + (ry)^2 + (rz)^2)^(5/2);
+k_ = 100;
+nPos = (rx)^2 + (ry)^2 + (rz)^2;
+consX =Lvx*(muEarth/nPos^(3/2) - (3*muEarth*rx*abs(rx)*sign(rx))/nPos^(5/2)) - (4*rho_s_*rx*f_)/(a^2*(exp(k_*f_) + 1)) - (3*Lvy*muEarth*ry*abs(rx)*sign(rx))/nPos^(5/2) - (3*Lvz*muEarth*rz*abs(rx)*sign(rx))/nPos^(5/2) + (2*k_*rho_s_*rx*exp(k_*f_)*f_^2)/(a^2*(exp(k_*f_) + 1)^2);
+consY =Lvy*(muEarth/nPos^(3/2) - (3*muEarth*ry*abs(ry)*sign(ry))/nPos^(5/2)) - (4*rho_s_*ry*f_)/(b^2*(exp(k_*f_) + 1)) - (3*Lvx*muEarth*rx*abs(ry)*sign(ry))/nPos^(5/2) - (3*Lvz*muEarth*rz*abs(ry)*sign(ry))/nPos^(5/2) + (2*k_*rho_s_*ry*exp(k_*f_)*f_^2)/(b^2*(exp(k_*f_) + 1)^2);
+consZ =                                               -(muEarth*(- Lvz*rx^2 + 3*Lvx*rx*rz - Lvz*ry^2 + 3*Lvy*ry*rz + 2*Lvz*rz^2))/(rx^2 + ry^2 + rz^2)^(5/2);
+ 
 ds(1:3) = [consX, consY, consZ];
 
 % ds(1) = (-(muEarth*(2*L(4)*x(1)^(2) + 3*L(5)*x(1)*x(2) + 3*L(6)*x(1)*x(3) - L(4)*x(2)^(2) - L(4)*x(3)^(2)))/nPos^(5/2) )/(1) ...
